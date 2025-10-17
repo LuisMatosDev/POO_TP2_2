014b21c7a81c44edfdf7263e67b4f7f9d01c630d
LuisMatosDev <kagenoryuu@gmail.com>
2025-10-17 15:40:03 +0100

    Adição de comentários nos métodos e classes do projecto.

--- src/main/java/oop/tp2_2/models/Partido.java
+++ src/main/java/oop/tp2_2/models/Partido.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- *
+ * Representa um partido ou coligação no sistema eleitoral.
+ * Guarda o nome, contagem de votos, alocação de assentos, e informação da coligação.
  * @author Luis Matos
  */
 public class Partido
@@ -15,6 +16,11 @@ public class Partido
     private boolean eColigacao;
     private List<String> membrosColigacao;
     
+    /**
+     * Constructor de partidos individuais.
+     * @param nome O nome do partido.
+     * @param votos O número de votos obtidos pelo partido.
+     */
     public Partido(String nome, int votos)
     {
         this.nome = nome;
@@ -25,6 +31,11 @@ public class Partido
         this.membrosColigacao.add(nome);
     }
     
+    /**
+     * Constructor para coligações, formadas a partir de partidos individuais.
+     * @param nomeColigacao O nome da coligação.
+     * @param membros Lista dos partidos individuais que formam a coligação.
+     */
     public Partido(String nomeColigacao, List<Partido> membros)
     {
         this.nome = nomeColigacao;
@@ -33,6 +44,7 @@ public class Partido
         this.votos = 0;
         this.assentos = 0;
         
+        // Agregação dos votos obtidos por todos os membros da coligação.
         for (Partido membro : membros)
         {
             this.votos += membro.getVotos();
@@ -72,11 +84,20 @@ public class Partido
         this.assentos = assentos;
     }
     
+    /**
+     * Incrementa a contagem do número de assentos para um dado partido por um.
+     * Utilizada durante a alocação de assentos de acordo com o método D'Hondt.
+     */
     public void adicionaAssento()
     {
         this.assentos++;
     }
     
+    /**
+     * Retorna a cadeia formatada que representa um partido.
+     * Para coligações, inclui os nomes dos partido membros da coligação.
+     * @return Cadeia formatada para exibir os nomes do partidos/coligações.
+     */
     @Override
     public String toString()
     {
